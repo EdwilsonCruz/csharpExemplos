370ea031f24ec3ea2ca5ab65638f6049ad03ae1b
edwilsoncruz <k2eddwilson@gmail.com>
2021-02-12 15:42:13 -0300

    Testando commit

--- Aula02/Program.cs
+++ Aula02/Program.cs
@@ -2,11 +2,11 @@
 
 namespace Aula02
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      Console.WriteLine("Lyncas Cursos!");
     }
+  }
 }
