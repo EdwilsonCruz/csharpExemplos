127aeb1e463792f74f342dfdc4ae6a5cf367ec8f
edwilsoncruz <k2eddwilson@gmail.com>
2021-03-01 15:00:28 -0300

    aulas 03/04/05/06

--- Aula02/Program.cs
+++ Aula02/Program.cs
@@ -4,15 +4,38 @@ namespace Aula02
 {
   class Program
   {
+    //variavel static , pra poder usar junto aos metodos static.
+    static int num = 10;
     static void Main(string[] args)
     {
       Console.WriteLine("Lyncas Cursos! ");
-      
+
       foreach (var item in args)
       {
-          Console.WriteLine(item);
+        Console.WriteLine(item);
       }
-      
+      Aula03();
+    }
+
+    private static void Aula03()
+    {
+      Console.WriteLine(num);
+      bool res = 10 > 5;
+      Console.WriteLine(res);
+      //Aula 04,05,06
+      double valorCompra = 5.60;
+      double valorVenda = 7.00;
+      double lucro = 0.2;
+      string produto = "Muda de Cacto";
+
+      Console.WriteLine("Produto....:{0,15}",produto);
+      Console.WriteLine("Vl.Compra..:{0,15:c}",valorCompra);
+      Console.WriteLine("Lucro......:{0,15:p}",lucro);
+      Console.WriteLine("Produto....:{0,15:c}",valorVenda);
+      Console.WriteLine("...........................");
+      var valorReal =  (valorVenda - valorCompra);
+      Console.WriteLine("Lucro Real.:{0,15:c}",valorReal);
+
     }
   }
 }
